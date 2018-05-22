74ef469326e67dea8ae27957546d28cb4549a408
João Pedro Gonçalves <joao.kamun@gmail.com>
2018-05-22 16:30:20 -0300

    Updated Generator, to fix colors

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const config = require('./config.json')
 
 const generate = async () => {
   const tachy = tachyonsGenerator(config)
-
   const out = await tachy.generate()
 
   for (const [key, value] of Object.entries(out.src)) {
     fs.writeFileSync(`src/${key}.css`, value)
   }
+
   fs.writeFileSync('index.html', out.docs)
   fs.writeFileSync('tachyons.css', out.css)
   fs.writeFileSync('tachyons.min.css', out.min)
